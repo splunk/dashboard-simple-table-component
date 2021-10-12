643c2be6c11b13b0c61e4b263e5dd4c7c44b9ae3
Nusair Haq <nhaq@splunk.com>
2021-10-12 18:00:50 -0400

    going through and adding licensing comments

--- packages/custom-table/src/CustomTable.jsx
+++ packages/custom-table/src/CustomTable.jsx
@@ -1,3 +1,18 @@
+/* Copyright 2021 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. */
+
+
 import React, { Component } from 'react';
 import Table from '@splunk/react-ui/Table';
 import Line from '@splunk/react-sparkline/Line';
@@ -6,38 +21,11 @@ import { cloneDeep } from 'lodash';
 import SplunkThemeProvider from "@splunk/themes/SplunkThemeProvider";
 
 
-/*const data = [
-    { User: 'userA', tl: 10, logins: [1,0,0,2,3,4,0] },
-    { User: 'userB', tl: 20, logins: [0,4,2,5,3,6,0] },
-    { User: 'userC', tl: 30, logins: [5,2,5,4,6,2,6] },
-    { User: 'userD', tl: 40, logins: [7,5,8,6,6,8,10] },
-]; */
-
 class AreaInTable extends Component {
     constructor(props, ...rest) {
         super(props, ...rest);
 
         this.state = {
-            /*showTooltip: true,
-            isArea: true,
-            showEndDot: true, 
-            endDotFillColor: 'white',
-            endDotRadius: 3,
-            endDotStroke: 'black',
-            endDotStrokeWidth: 2,
-            endDotCount: 0,
-            fillColor: '#65a637',
-            fillOpacity: 0.1,
-            cursorStroke: '#f7912c',
-            cursorStrokeWidth: 3,
-            cursorStrokeLinecap: 'square',
-            cursorStrokeDasharray: '1,5',
-            height: 50,
-            lineColor: '#6cb8ca',
-            lineStrokeWidth: 4,
-            lineLength: 10,
-            width: 240, */
-
             data :[
                 { User: 'userA', tl: 10, logins: [1,0,0,2,3,4,0] },
                 { User: 'userB', tl: 20, logins: [0,4,2,5,3,6,0] },
