049451a1bc99c9dc2c9b6a73fe8f9f47b930f50a
Nusair Haq <nhaq@splunk.com>
2022-11-01 11:01:00 -0400

    compressed preset for smaller bundle

--- packages/table-dashboard/src/main/webapp/pages/dashboard/DashboardExample.jsx
+++ packages/table-dashboard/src/main/webapp/pages/dashboard/DashboardExample.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import DashboardCore from '@splunk/dashboard-core';
 import { DashboardContextProvider } from '@splunk/dashboard-context';
-import EnterpriseViewOnlyPreset from '@splunk/dashboard-presets/EnterpriseViewOnlyPreset';
+// import EnterpriseViewOnlyPreset from '@splunk/dashboard-presets/EnterpriseViewOnlyPreset';
+import Table from '@splunk/visualizations/Table';
+import Markdown from '@splunk/visualizations/Markdown';
+import Rectangle from '@splunk/visualizations/Rectangle';
 import SplunkThemeProvider from '@splunk/themes/SplunkThemeProvider';
-
+import AbsoluteLayoutViewer from '@splunk/dashboard-layouts/AbsoluteLayoutViewer';
 import CustomTable from '@splunk/custom-table';
+import TestDataSource from '@splunk/datasources/TestDataSource';
+
+// import CustomTable from '@splunk/my-react-component';
 import definition from './definition.json';
 
 const themeToVariant = {
@@ -16,21 +22,29 @@ const themeToVariant = {
 
 // use DashboardCore to render a simple dashboard
 const customPreset = {
-    ...EnterpriseViewOnlyPreset,
+    layouts: {
+        absolute: AbsoluteLayoutViewer,
+    },
     visualizations: {
-        ...EnterpriseViewOnlyPreset.visualizations,
+        'splunk.table': Table,
+        'splunk.markdown': Markdown,
+        'splunk.rectangle': Rectangle,
         'splunk.CustomTable': CustomTable,
     },
+    dataSources: {
+        'ds.test': TestDataSource,
+    },
 };
-
+// NOTICE: If you are on Dashboard Core versions LOWER than 25.X.X, preset will be a prop for <DashboardCore> instead of
+// <DashboardContextProvider>
 const DashboardExample = () => {
     return (
         <SplunkThemeProvider {...themeToVariant.prisma}>
             <DashboardContextProvider>
                 <DashboardCore
+                    preset={customPreset}
                     width="100%"
                     height="100%"
-                    preset={customPreset}
                     definition={definition}
                 />
             </DashboardContextProvider>
